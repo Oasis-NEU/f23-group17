0436de6987a87a2961f09a2007aeb2b171366db9
Sharon Yang <sharonyyang0@gmail.com>
2023-10-29 08:37:59 -0400

    Created Previews component

--- khoury-class-recs/src/components/CourseDescPreview.js
+++ khoury-class-recs/src/components/CourseDescPreview.js
@@ -1,5 +1,13 @@
 import "./comp-style.css";
 
+/**
+ * props:
+ *  name - string: name of the class
+ *  desc - string: the class's description
+ *  rating - string: current rating out of 5
+ *  preReqs - array of strings: the prereqs for this class
+ *  coReqs - array of string: the coreqs for this class
+ */
 export default function CourseDescPreview(props) {
   return (
     <div className="course-desc-wrapper">
