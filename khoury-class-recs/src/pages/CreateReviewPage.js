c97f1ba379ac25a921ab5fcc39a219fdc02b1557
Sharon Yang <sharonyyang0@gmail.com>
2023-11-08 20:36:04 -0500

    Comment outline for review page

---
+++ khoury-class-recs/src/pages/CreateReviewPage.js
@@ -0,0 +1,17 @@
+export default function CreateReviewPage() {
+  /**
+   * select from drop down which class you want to review
+   * - should only include classes you've taken + classes you haven't already reviewed
+   * - once selected, form appears
+   * - if you select a different option for the drop down, the form should clear all the
+   *   input fields and update the form to say the new selected class
+   */
+  /**
+   * Form
+   * - Header: "Review CSXXXX"
+   * - Difficulty: MUI NumberInput
+   * - Workload: MUI NumberInput
+   * - Overall Rating: MUI Rating
+   * - Additional Comments (optional): MUI Textarea
+   */
+}
